422c5be1e304b289efddcf048b46fbdd5fbee878
vishnuvs369 <vishnuvs769@gmail.com>
2022-08-23 15:14:06 +0530

    first commit

---
+++ database/db.php
@@ -0,0 +1,7 @@
+<?php
+	$database_username = 'root';//user name
+	$database_password = '';//password
+	$pdo_conn = new PDO( 'mysql:host=localhost;dbname=blog_admin_db', $database_username, $database_password );
+?>
+
+
